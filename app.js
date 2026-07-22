895cfd831d0d783fc1b9a3b25c0a483aeead80c7
namita-sham <namita.sham1@umail.uom.ac.mu>
2026-07-23 01:16:12 +0400

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const API_KEY = "ghp_DEMO_FAKE_SECRET_123456789";
+const API_KEY = "AKIAIOSFODNN7EXAMPLE";
 
 app.get("/", (req, res) => {
     res.send("Welcome to the vulnerable application");
